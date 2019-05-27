8c860f62255f35b6045b818ebc3ff2453d489337
Matthew Fair <matthew.fair@queensu.ca>
2019-05-27 11:35:12 -0400

    Add quiz.js

---
+++ quiz.js
@@ -0,0 +1,102 @@
+quizStc = [
+	{
+		"question" : "What's your idea of a perfect night?",
+		"answers" : [
+			["A quiet night at home","2","1","3","4","0"],
+			["Writing, painting, or otherwise creating","3","2","1","0","4"],
+			["Planning your future successes","4","0","3","2","1"],
+			["Trying a new restaurant","1","2","4","0","3"],
+			["Time with friends","0","4","3","1","2"],
+		]
+	},
+	{
+		"question" : "Choose a superpower:",
+		"answers" : [
+			["Flight","1","2","4","0","3"],
+			["Reading minds","1","0","2","3","4"],
+			["Invulnerability","3","2","1","4","0"],
+			["Super-Intelligence","4","3","2","0","1"],
+			["Mind control","0","4","3","2","1"],
+		]
+	},
+	{
+		"question" : "What motivates you?",
+		"answers" : [
+			["Money","4","0","3","2","1"],
+			["Job satisfaction","3","4","2","1","0"],
+			["Work/life balance","0","4","3","2","1"],
+			["Family","1","3","0","4","2"],
+			["Creative freedom","3","1","2","0","4"],
+		]
+	},
+	{
+		"question" : "How do you solve a problem?",
+		"answers" : [
+			["Hire somebody to solve it","4","0","2","3","1"],
+			["Invent a solution","3","2","1","0","4"],
+			["Ask others for help","0","4","3","1","2"],
+			["Do without or work around it","1","3","2","4","0"],
+			["Research it exhaustively","1","3","4","2","0"],
+		]
+	},
+	{
+		"question" : "If somebody gave you $5000, how would you spend it?",
+		"answers" : [
+			["Start a new small business","4","0","1","2","3"],
+			["Support a charity","1","4","0","3","2"],
+			["Put it in long-term savings","3","1","2","4","0"],
+			["Plan a vacation","2","3","4","0","1"],
+			["Buy new tools, instruments, or art supplies","2","3","0","1","4"],
+		]
+	},
+	{
+		"question" : "What do you do at parties?",
+		"answers" : [
+			["Learn as much as I can about everyone I talk to","4","3","0","1","2"],
+			["Talk to people who look as uncomfortable as I am","0","4","1","3","2"],
+			["Seek out interesting people to chat with","2","3","4","0","1"],
+			["Find people I know and spend my time with them","1","2","3","4","0"],
+			["Start a singalong, group activity, or conga line","3","2","1","0","4"],
+		]
+	},
+	{
+		"question" : "What's the most important thing about a new idea?",
+		"answers" : [
+			["Will it succeed and be profitable?","4","1","2","3","0"],
+			["Will it help people?","0","4","1","2","3"],
+			["Does it lead to new inventions or ideas?","3","2","4","0","1"],
+			["Does it support something I value?","1","3","0","4","2"],
+			["Is it out-of-the-box creative and different?","2","0","1","3","4"],
+		]
+	},
+	{
+		"question" : "Your friend has a problem at work. What do you do?",
+		"answers" : [
+			["Encourage them to strike out on their own","4","1","3","0","2"],
+			["Empathize and console them","2","4","0","3","1"],
+			["Ask if they've considered switching careers","1","3","4","2","0"],
+			["Tell them to stick with it and weather the storm","0","2","1","4","3"],
+			["Come up with ideas to solve it","3","0","2","1","4"],
+		]
+	},
+	{
+		"question" : "What's a must-have quality for a new friend?",
+		"answers" : [
+			["Knowledge and skills I don't have","4","3","2","0","1"],
+			["Compassion and empathy","1","4","2","3","0"],
+			["Openness to adventure","2","1","4","0","3"],
+			["Traditional and shared values","0","2","3","4","1"],
+			["Creativity and expressiveness","3","0","1","2","4"],
+		]
+	},
+	{
+		"question" : "What's your leadership style?",
+		"answers" : [
+			["Dynamic: I'm out front and providing vision","4","0","3","1","2"],
+			["I make sure everyone feels valued and can contribute","3","4","2","0","1"],
+			["I seek consensus, but everyone knows I'm in charge","2","1","4","3","0"],
+			["I set achievable goals with firm deadlines","0","2","1","4","3"],
+			["I set as few restrictions and rules as possible","1","0","3","2","4"],
+		]
+	},
+];
